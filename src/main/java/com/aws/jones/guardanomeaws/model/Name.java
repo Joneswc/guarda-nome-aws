625ca5af59af6f9688f9e8938f8c2b8f00d1407c
Joneswc <joneswc@gmail.com>
2020-04-16 21:30:27 -0300

    adicionar a serializacao do dado entregue pela API

--- src/main/java/com/aws/jones/guardanomeaws/model/Name.java
+++ src/main/java/com/aws/jones/guardanomeaws/model/Name.java
@@ -8,12 +8,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
-public class Name {
+public class Name implements Serializable {
+
+    public static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO )
